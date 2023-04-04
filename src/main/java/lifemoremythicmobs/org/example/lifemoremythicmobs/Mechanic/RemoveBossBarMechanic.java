946594b1919b1735df742b078f077aa0d015717d
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-04-05 05:32:18 +0900

    なんか変えた

--- src/main/java/lifemoremythicmobs/org/example/lifemoremythicmobs/Mechanic/RemoveBossBarMechanic.java
+++ src/main/java/lifemoremythicmobs/org/example/lifemoremythicmobs/Mechanic/RemoveBossBarMechanic.java
@@ -6,13 +6,17 @@ import io.lumine.xikage.mythicmobs.io.MythicLineConfig;
 import io.lumine.xikage.mythicmobs.skills.ITargetedEntitySkill;
 import io.lumine.xikage.mythicmobs.skills.SkillMechanic;
 import io.lumine.xikage.mythicmobs.skills.SkillMetadata;
-import lifemoremythicmobs.org.example.lifemoremythicmobs.Util.BossBarUtil;
 import org.bukkit.entity.Player;
 
 public class RemoveBossBarMechanic extends SkillMechanic implements ITargetedEntitySkill {
+
+    protected final String id;
+
     public RemoveBossBarMechanic(MythicLineConfig config) {
 
         super(config.getLine(), config);
+
+        this.id = config.getString(new String[]{"id", "i"}, "def");
 
     }
 
@@ -20,7 +24,7 @@ public class RemoveBossBarMechanic extends SkillMechanic implements ITargetedEnt
     public boolean castAtEntity(SkillMetadata skillMetadata, AbstractEntity target) {
         Player bukkitTarget = (Player) BukkitAdapter.adapt(target);
 
-        BossBarUtil.removeBossBar(bukkitTarget);
+        BossBarMechanic.bars.get(id).removePlayer(bukkitTarget);
 
         return true;
     }
