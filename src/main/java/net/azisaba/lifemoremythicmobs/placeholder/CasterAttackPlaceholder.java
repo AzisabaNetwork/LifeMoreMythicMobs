a537274f329b21c7be5809114952dd640ead3ec5
Sirnk <147326106+Sirnk@users.noreply.github.com>
2024-02-10 15:49:59 +0900

    初回コミット+ <caster.armor>と<caster.attack>の追加

---
+++ src/main/java/net/azisaba/lifemoremythicmobs/placeholder/CasterAttackPlaceholder.java
@@ -0,0 +1,27 @@
+package net.azisaba.lifemoremythicmobs.placeholder;
+
+import io.lumine.xikage.mythicmobs.adapters.bukkit.BukkitAdapter;
+import io.lumine.xikage.mythicmobs.skills.placeholders.Placeholder;
+import io.lumine.xikage.mythicmobs.skills.placeholders.PlaceholderManager;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+
+import static io.lumine.xikage.mythicmobs.MythicMobs.inst;
+
+public class CasterAttackPlaceholder {
+    public static void attackRegister() {
+        PlaceholderManager manager = inst().getPlaceholderManager();
+        manager.register("caster.attack", Placeholder.meta(((placeholderMeta, s) -> {
+            Entity entity = BukkitAdapter.adapt(placeholderMeta.getCaster().getEntity());
+            if (entity instanceof LivingEntity) {
+                LivingEntity living = (LivingEntity) entity;
+                AttributeInstance attr = living.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
+                if (attr == null) return "0.0";
+                return String.valueOf((int) attr.getValue());
+            }
+            return "0.0";
+        })));
+    }
+}
